a72caebc4dd58b64abe607326d118c71a20c6b20
Steven Moore <stevenallenmoore@gmail.com>
2020-02-17 11:59:35 -0500

    Updated for Spring 2020

---
+++ README.md
@@ -0,0 +1,25 @@
+#### CS 120
+# Lab 3 - Working with Binary in C
+
+### [Assignment Description](https://docs.google.com/document/d/190Hnk0Z_HKicqwh_e-uiW2_0uUSP428_xYf1D1tnDzQ/edit?usp=sharing)
+
+***
+
+_Replace anything surrounded by the `< >` symbols._
+
+## SUMMARY:
+ < Provide a short description of your program's functionality. No more than a couple sentences. >
+
+## GRACE DAYS
+Grace days used for this assignment: < # >
+
+Grace days remaining: < # >/5
+
+## KNOWN BUGS AND INCOMPLETE PARTS:
+ < What parts of the project you were not able to complete >
+
+## REFERENCES:
+ < List any outside resources used >
+
+## MISCELLANEOUS COMMENTS:
+ < Anything you would like the grader to know >
---
+++ lab3.c
@@ -0,0 +1,60 @@
+#include <limits.h>
+#include <stdio.h>
+#include <assert.h>
+
+unsigned countOnes(int num, int bin[32]){
+    /*Your Code here*/
+}
+
+void printBinary(int num){
+    /*Your Code here*/
+}
+
+/**************** Do not touch anything after this comment *************/
+
+int compareArrays(int arr1[32], int arr2[32]){
+    for(int i = 0; i < sizeof(int); i++){
+        if(arr1[i] != arr2[i]) return 0;
+    }
+    retunr 1;
+}
+int main(){
+    ///////// PART A Tests /////////
+    assert(countOnes(2) == 1);
+    assert(countOnes(255) == 8);
+    assert(countOnes(-1) == 32);
+    assert(countOnes(INT_MAX) == 31);
+    assert(countOnes(INT_MIN) == 1);
+
+    ///////// PART B TESTS /////////
+    int binary[32];
+
+    int testvalue = 1;
+    int test1[32] = {0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,1};
+    printf("Testing Binary for Integer Value: %d\n", testvalue);
+    printBinary(testvalue, binary);
+    assert(compareArrays(test, binary);
+    printf("\t...Test Passed!");
+
+    int testvalue = 255;
+    int test1[32] = {0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 1,1,1,1, 1,1,1,1};
+    printf("Testing Binary for Integer Value: %d\n", testvalue);
+    printBinary(testvalue, binary);
+    assert(compareArrays(test, binary);
+    printf("\t...Test Passed!");
+
+    int testvalue = 1431655765;
+    int test1[32] = {0,1,0,1, 0,1,0,1, 0,1,0,1, 0,1,0,1, 0,1,0,1, 0,1,0,1, 0,1,0,1, 0,1,0,1};
+    printf("Testing Binary for Integer Value: %d\n", testvalue);
+    printBinary(testvalue, binary);
+    assert(compareArrays(test, binary);
+    printf("\t...Test Passed!");
+
+    int testvalue = -1;
+    int test1[32] = {1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1};
+    printf("Testing Binary for Integer Value: %d\n", testvalue);
+    printBinary(testvalue, binary);
+    assert(compareArrays(test, binary);
+
+    printf("\tAll Tests Passed! Make sure to push to Github and submit your hash on MyCourses!");
+}
